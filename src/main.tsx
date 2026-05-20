696bee0e0518dea7dbaec16920d0c579bf902a4f
agung3956 <agung3956@gmail.com>
2026-05-20 23:54:54 +0700

    Fix GitHub Pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 
+const basename = window.location.pathname.startsWith('/EWS') ? '/EWS' : undefined
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
