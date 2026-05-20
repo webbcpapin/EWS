c2d216b5f7d67e03b72e22aa6fac25947987c33c
agung3956 <agung3956@gmail.com>
2026-05-20 23:58:41 +0700

    Use hash routing for Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 
-const basename = window.location.pathname.startsWith('/EWS') ? '/EWS' : undefined
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
